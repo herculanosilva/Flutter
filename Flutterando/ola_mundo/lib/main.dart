81b34e72af99a6d3fb2307075acf86b55ca366b9
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-30 11:16:31 -0300

    MaterialApp
    Aula 07

--- Flutterando/ola_mundo/lib/main.dart
+++ Flutterando/ola_mundo/lib/main.dart
@@ -12,14 +12,11 @@ class AppWidget extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title, 
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.0),
-        ),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: Container(
+        child: Center(child: Text('Fluterando')),
       ),
-    ); 
+    );
   }
 }
