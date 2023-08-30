0e82dc2947ab7e764c84b92e21ea6eeb1f961897
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-30 11:08:36 -0300

    StatelessWidget
    Aula 06

--- Flutterando/ola_mundo/lib/main.dart
+++ Flutterando/ola_mundo/lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 main(){
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Flutterando', 
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
+  runApp(AppWidget(title: 'Flutterando'));
+  }
+
+class AppWidget extends StatelessWidget{
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title, 
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
       ),
-    ),
-  ));
+    ); 
+  }
 }
