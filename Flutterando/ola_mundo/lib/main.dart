d432470a2858799a3918b51bfa7a615b98c05586
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-30 14:38:47 -0300

    StatefulWidget
    Aula 08

--- Flutterando/ola_mundo/lib/main.dart
+++ Flutterando/ola_mundo/lib/main.dart
@@ -9,14 +9,36 @@ class AppWidget extends StatelessWidget{
 
   const AppWidget({super.key, required this.title});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: Container(
-        child: Center(child: Text('Fluterando')),
-      ),
+      home: HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage>{
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+        child: Center(child: GestureDetector(
+          child: Text('Contador interativo : $counter'), 
+          onTap: (){
+          setState(() {
+            counter++;
+          });
+        },)),
+  );
+  }
+
 }
