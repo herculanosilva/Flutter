8b16709832d857dc41094923a191e968799f5bd5
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-30 15:31:42 -0300

    Boas praticas e organização
    Aula 09

---
+++ Flutterando/ola_mundo/lib/app_widget.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: HomePage(),
+    );
+  }
+}
--- Flutterando/ola_mundo/lib/main.dart
+++ Flutterando/ola_mundo/lib/main.dart
@@ -1,44 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'app_widget.dart';
 main(){
-  runApp(AppWidget(title: 'Flutterando'));
+  runApp(AppWidget());
   }
 
-class AppWidget extends StatelessWidget{
-  final String title;
 
-  const AppWidget({super.key, required this.title});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState(){
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage>{
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-  return Container(
-        child: Center(child: GestureDetector(
-          child: Text('Contador interativo : $counter'), 
-          onTap: (){
-          setState(() {
-            counter++;
-          });
-        },)),
-  );
-  }
-
-}
