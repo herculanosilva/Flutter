e7d82d6d2a8bc01f0da38fc96a450510a67c9ec0
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-31 10:55:32 -0300

    Gerenciando outro estado
    Aula 12

--- Flutterando/ola_mundo/lib/app_widget.dart
+++ Flutterando/ola_mundo/lib/app_widget.dart
@@ -1,15 +1,26 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: HomePage(),
+    return AnimatedBuilder(
+      animation: AppController.instance,
+        builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: AppController.instance.isDarkTheme 
+              ? Brightness.dark 
+              : Brightness.light,
+          ),
+          home: HomePage(),
+        );
+      },
     );
   }
 }
