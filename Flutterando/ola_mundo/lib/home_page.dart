694c6b95a1e10d9b99983604d35f5fa7a47e0429
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-08-31 11:44:22 -0300

    Columns
    Aula 14

--- Flutterando/ola_mundo/lib/home_page.dart
+++ Flutterando/ola_mundo/lib/home_page.dart
@@ -19,8 +19,16 @@ class HomePageState extends State<HomePage>{
       title: Text('Home Page'),
       actions: [CustomSwitch()],
       ),
-    body: Center(
-      child: CustomSwitch()
+    body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Contador: $counter'),
+          CustomSwitch(),
+        ],
+      ),
     ),
     floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
