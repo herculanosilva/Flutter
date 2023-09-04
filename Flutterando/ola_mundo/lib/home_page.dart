26a534e841e0c8023a3572fc18ca51d5146789c1
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-09-04 15:19:44 -0300

    Drawe
    Aula 25

--- Flutterando/ola_mundo/lib/home_page.dart
+++ Flutterando/ola_mundo/lib/home_page.dart
@@ -1,78 +1,104 @@
-
 import 'package:flutter/material.dart';
 import 'package:ola_mundo/app_controller.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState(){
+  State<StatefulWidget> createState() {
     return HomePageState();
   }
 }
 
-class HomePageState extends State<HomePage>{
+class HomePageState extends State<HomePage> {
   int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Home Page'),
-      actions: [CustomSwitch()],
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: Image.network(
+                  'https://media.licdn.com/dms/image/C4E03AQFA0b1hYDGv3g/profile-displayphoto-shrink_800_800/0/1539398100010?e=1699488000&v=beta&t=cvMfy610wzifFcK02UjM8NeRPQ0l2ZipJF63t21EISY'),
+              accountName: Text('Herculano'),
+              accountEmail: Text('herculano@email.com'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('logout'),
+              subtitle: Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            ),
+          ],
+        ),
       ),
-    body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Contador: $counter'),
-          Container(height: 50),
-          CustomSwitch(),
-          Container(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.deepOrange,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.deepOrangeAccent,
-              ),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.orange,
-              ),
-            ],
-          )
-        ],
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [CustomSwitch()],
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Contador: $counter'),
+            Container(height: 50),
+            CustomSwitch(),
+            Container(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.deepOrange,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.deepOrangeAccent,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.orange,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          setState(() {
-            counter++;
-          },);
+          setState(
+            () {
+              counter++;
+            },
+          );
         },
       ),
-  );
+    );
   }
-
 }
 
 class CustomSwitch extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Switch(
-        value: AppController.instance.isDarkTheme, 
+        value: AppController.instance.isDarkTheme,
         onChanged: (value) {
           AppController.instance.changeTheme();
-      });
+        });
   }
 }
