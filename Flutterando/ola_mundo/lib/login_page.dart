b51d8e9b12bd80dda7b3c3b7dce466460c2b7164
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-09-04 11:21:15 -0300

    Material Card
    Aula 24

--- Flutterando/ola_mundo/lib/login_page.dart
+++ Flutterando/ola_mundo/lib/login_page.dart
@@ -9,64 +9,84 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   String email = '';
   String password = '';
 
-  Widget _body(){
+  Widget _body() {
     return SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 200,
-                  height: 150,
-                  child: Image.asset(
-                    'assets/images/logo.png'
-                    ),
-                ),
-                Container(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(), 
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(height: 20),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12, right: 12, top: 20, bottom: 12),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          if (email == 'herculano@email.com' &&
+                              password == '123') {
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print('email ou senha incorreto');
+                          }
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          child: Text(
+                            'Entrar',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(), 
-                  ),
-                ),
-                SizedBox(height: 15,),
-                ElevatedButton(onPressed: () {
-                  if(email == 'herculano@email.com' && password == '123'){
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  }else {
-                    print('email ou senha incorreto');
-                  }
-                },
-                  child: Text('Entrar'),
-                  ),
-                ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 
   @override
@@ -75,12 +95,14 @@ class _LoginPageState extends State<LoginPage> {
       body: Stack(
         children: [
           SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Image.asset('assets/images/background.jpg', 
-            fit: BoxFit.cover,
-            )),
-            Container(color: Colors.black.withOpacity(0.2),
-            ),
+              height: MediaQuery.of(context).size.height,
+              child: Image.asset(
+                'assets/images/background.jpg',
+                fit: BoxFit.cover,
+              )),
+          Container(
+            color: Colors.black.withOpacity(0.2),
+          ),
           _body(),
         ],
       ),
