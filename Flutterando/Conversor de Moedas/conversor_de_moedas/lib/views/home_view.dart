e45e07d974d71fd02a03344b76102e3bd7827207
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-09-05 18:25:20 -0300

    Desenhando a View
    Aula 30

---
+++ Flutterando/Conversor de Moedas/conversor_de_moedas/lib/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Image.asset('assets/images/logo.png'),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              children: [
+                Expanded(child: DropdownButton(items: [], onChanged: (value) {})),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Converter'),
+          )
+          ],
+        ),
+      ),
+    );
+  }
+}
