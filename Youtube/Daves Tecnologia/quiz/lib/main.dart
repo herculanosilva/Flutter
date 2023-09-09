01743ea5ece506fd8390a0f3f5d6304f1edb09eb
Herculano Silva <herculano.silva@comparedistribuidora.com.br>
2023-09-09 11:56:01 -0300

    iniciando o projeto
    Jogo Quiz

---
+++ Youtube/Daves Tecnologia/quiz/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz - Curso de Flutter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const FlutterLogo(size: 300),
+              ElevatedButton(
+                onPressed: (){
+                  print('precionado');
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.fromLTRB(100, 20, 100, 20),
+                ), 
+                child: const Text('Jogar', style: TextStyle(fontSize: 50),),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
